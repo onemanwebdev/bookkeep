831af5ead562e4c2c79157308c122ba520fa66b2
Mateusz Boroch <mateusz.boroch@rp.pl>
2017-06-26 16:43:13 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const css = require('./scss/style.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './js/index';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Loader from './loader'
+
+class App extends React.Component {
+    render() {
+        return (
+            <Loader />
+        )
+    }
+}
+
+export default App;
